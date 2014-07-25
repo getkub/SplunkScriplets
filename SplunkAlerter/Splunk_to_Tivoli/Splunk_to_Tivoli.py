09f3ab4fddf185c6c519762b831e0d2bde13cfa9
getkub <getkub@users.noreply.github.com>
2014-07-25 16:29:07 +0100

    Updated to use Arrays to send alerts to Tivoli

--- SplunkAlerter/Splunk_to_Tivoli/Splunk_to_Tivoli.py
+++ SplunkAlerter/Splunk_to_Tivoli/Splunk_to_Tivoli.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python
 import csv, time
 import os, subprocess
@@ -10,14 +11,19 @@ import logging
 # Name:            Splunk_to_Tivoli.py
 #
 program =    sys.argv[0]
-version =    "1"
-verdate =    "2014-03-04T13:02:00Z"
+version =    "2"
+verdate =    "2014-05-23T13:02:00Z"
 #
 #
-# Purpose:     Script to provide the interface to postzmsg to send onto Omnibus. 
+# Purpose:     Script to provide the interface to postzmsg to send onto Omnibus.
 #            The script is called from the splunk saved Search and will parse
 #            csv file with specific Headers.
 #
+# Version History
+#
+#     Version    Date      Comment
+#       1       2014-03-04 Initial Version
+#       2       2014-05-23 Changed logic to merge Array as Omnibus not detecting quotes
 #
 # *************************************************************************************
 
@@ -25,23 +31,30 @@ verdate =    "2014-03-04T13:02:00Z"
 # Logging Configurations
 # *************************************************************************************
 TODAY_YYYYMMDD     =    time.strftime("%Y%m%d")
-LOG_DIR            =    '/tmp/'
+APP_DIR            =    os.path.dirname(os.path.dirname(os.path.dirname(program)))
+LOG_DIR            =    APP_DIR + '/logs'
 LOG_FILENAME       =    'Splunk_to_Tivoli.' + TODAY_YYYYMMDD + '.log'
-LOG_ABSFILE        =    LOG_DIR + LOG_FILENAME
+LOG_ABSFILE        =    LOG_DIR + '/' + LOG_FILENAME
 logging.basicConfig(filename=LOG_ABSFILE, format='%(asctime)s - %(levelname)s - [%(process)d] - %(message)s', level=logging.INFO)
 
 # *************************************************************************************
 # Script Info
 # *************************************************************************************
 INPUT_PARAMS_LENGTH       =     len(sys.argv)
-if(len(sys.argv) < 2):
-    print "You must set argument!!!"
-    print "Usage is : <scriptname> <inputFile>"
-    logging.error('Wrong Usage: Input CSV File not specified')
+logging.debug('Number of arguments=' + str(INPUT_PARAMS_LENGTH))
+logging.debug('Argument list:' + str(sys.argv))
+# Splunk Documentation: http://docs.splunk.com/Documentation/Splunk/6.1.1/Alert/Configuringscriptedalerts
+
+if(len(sys.argv) < 9):
+    print "You must set 9 arguments!!"
+    print "Usage is : <scriptname> <arg1> <arg2> <arg3> <arg4> <arg5> <arg5> <arg6> <arg7> <arg8> <arg9>"
+    logging.error('Wrong Usage - Seems Script Not triggered via Splunk')
     exit(2)
-    
-CSV_FILE_IN                =    sys.argv[1]
-TEC_Source                 =    'Splunk_to_Tivoli'
+
+CSV_FILE_IN                =    sys.argv[4] + '.csv'
+COUNT_ALERTS               =    sys.argv[1]
+UNIQUE_URL                 =    sys.argv[6]
+TEC_Source                 =    'Splunk_to_Tivoli_v4'
 # *************************************************************************************
 # Script Parameters
 # *************************************************************************************
@@ -57,25 +70,25 @@ CSV_FILE_IN_ABS      = CSV_DIR + CSV_FILE_IN
 # Add more checks/validations to create stable script
 # *************************************************************************************
 
-logging.info('Starting Script ' + program)
+logging.info('Starting Script: ' + program)
 
-# Check if SPLUNK has been created required input file. 
+# Check if SPLUNK has been created required input file.
 # IF no file, then assume no alert to parse/send and exit
 if not os.path.isfile(CSV_FILE_IN_ABS):
-    logging.info('File ' + CSV_FILE_IN_ABS + ' NOT present. NO alerts to send')
-	logging.info('Ending Script ' + program)
+    logging.info('File: ' + CSV_FILE_IN_ABS + ' NOT present. NO alerts to send')
+    logging.info('Ending Script: ' + program)
     sys.exit()
 
 # Checking if Tivoli scripts/Configs are present. IF NOT throw error and exit
 if not os.path.isfile(TIVOLI_SCRIPT):
-    logging.error('File ' + TIVOLI_SCRIPT + ' NOT present. Exiting without sending alerts')
+    logging.error('File: ' + TIVOLI_SCRIPT + ' NOT present. Exiting without sending alerts')
     sys.exit()
-    
-if not os.path.isfile(TIVOLI_CFG):
-    logging.error('File ' + TIVOLI_CFG + ' NOT present. Exiting without sending alerts')
-    sys.exit()    
 
-# Creates Temporary Dir and Temporary file    
+if not os.path.isfile(TIVOLI_CFG):
+    logging.error('File: ' + TIVOLI_CFG + ' NOT present. Exiting without sending alerts')
+    sys.exit()
+
+# Creates Temporary Dir and Temporary file
 # Temporary file to copy CSV file and work with and delete
 # This would have of format <filename>.<timestamp>.tmp to accomodate multiple runs
 ts                      =    time.time()
@@ -87,14 +100,14 @@ try:
     subprocess.call(['mkdir','-p',TEMP_DIR])
     subprocess.call(['cp',CSV_FILE_IN_ABS,TEMP_FILE])
 except OSError:
-    logging.error('Unable to Copy Temp file ' + TEMP_FILE + ' . May be permission issue. Exiting without any action')
+    logging.error('Unable to Copy Temp file: ' + TEMP_FILE + ' . May be permission issue. Exiting without any action')
     sys.exit(2)
 
 # *************************************************************************************
 # Mandatory fields to Tivoli postzmsg
 # http://publib.boulder.ibm.com/tividd/td/tec/SC32-1232-00/en_US/HTML/ecormst33.htm
 # This script expects input file with first 3 fields mandatory
-# Severity,Message,AlertGroup,<field_4>,<field_5> .... <field_n> 
+# Severity,Message,AlertGroup,<field_4>,<field_5> .... <field_n>
 # *************************************************************************************
 
 # *************************************************************************************
@@ -102,7 +115,8 @@ except OSError:
 # Bit clunky, but to extract headers and real Data, I'm reading the input file twice !!
 # *************************************************************************************
 # countLines = len(open(TEMP_FILE).readlines(  ))  # Not required as CSV file is produced only if there is data
-logging.info('Start Sending Alerts using ' + TIVOLI_SCRIPT)
+logging.info('Start Sending Alerts using: ' + TIVOLI_SCRIPT + ', File=' + CSV_FILE_IN_ABS)
+logging.info('ALERTCOUNT=' + str(COUNT_ALERTS) + ' ,URL=' + str(UNIQUE_URL))
 
 # Reading Headers
 with open(TEMP_FILE) as hdr:
@@ -114,9 +128,9 @@ with open(TEMP_FILE) as hdr:
         while (count < ncol):
             headerVal.append(row[count])
             count = count + 1
-        
+
         break    # Break from "for" loop
-        
+
 # Reading real data/values
 with open(TEMP_FILE) as f:
     cf = csv.reader(f)
@@ -127,38 +141,38 @@ with open(TEMP_FILE) as f:
         while (count < ncol):
             realVal.append(row[count])
             count = count + 1
-            
-        CAT_TEC_Severity          =    '-r ' + realVal[0]
-        CAT_TEC_Message           =    '-m "' + realVal[1] + '"'
-        CAT_TEC_AlertGroup        =    realVal[2]
-        
-        # Rest of the fields, could be anything. So concatenating everything to one String
+
+        TEC_Severity          =    realVal[0]
+        TEC_Message           =    realVal[1]
+        TEC_AlertGroup        =    realVal[2]
+
+        # Rest of the fields, could be anything. So putting all of then into single array
         count    = 3
-        CAT_ALLOTHER_FIELDS            =    ''
+        OTHER_FIELDS_ARRAY            =    []
         while (count < ncol):
-            CAT_ALLOTHER_FIELDS        =     CAT_ALLOTHER_FIELDS  + headerVal[count] + '="' + realVal[count] + '" '
+            OTHER_FIELDS_ARRAY.append(headerVal[count] + '=' + realVal[count])
             count = count + 1
-        
-        # command = ['echo',TIVOLI_SCRIPT,'-f',TIVOLI_CFG,CAT_TEC_Severity,CAT_TEC_Message,CAT_ALLOTHER_FIELDS,CAT_TEC_AlertGroup,TEC_Source]
-        command = [TIVOLI_SCRIPT,'-f',TIVOLI_CFG,CAT_TEC_Severity,CAT_TEC_Message,CAT_ALLOTHER_FIELDS,CAT_TEC_AlertGroup,TEC_Source]
+
+        FINAL_ARRAY = [TIVOLI_SCRIPT,'-f',TIVOLI_CFG,'-r',TEC_Severity,'-m',TEC_Message] + OTHER_FIELDS_ARRAY[:] + [TEC_AlertGroup,TEC_Source]
+        logging.debug(FINAL_ARRAY[:])
         try:
-            subprocess.Popen(command)
+           p = subprocess.Popen(FINAL_ARRAY)
         except OSError:
-            logging.error('Unable to run Tivoli Script. Temp file ' + TEMP_FILE + ' not removed')
+            logging.error('Unable to run Tivoli Script. Temp file: ' + TEMP_FILE + ' not removed')
             sys.exit(2)
 
-logging.info('Finished Sending Alerts using ' + TIVOLI_SCRIPT)
+
+logging.info('Finished Sending Alerts using: ' + TIVOLI_SCRIPT)
 # End of Core logic ***************
 
 # Remove any temporary files created
 try:
     subprocess.call(['rm','-f',TEMP_FILE])
 except OSError:
-    logging.error('Unable to Remove Temp file ' + TEMP_FILE + '. Remove this manually')
+    logging.error('Unable to Remove Temp file: ' + TEMP_FILE + '. Remove this manually')
     sys.exit(2)
 
-logging.info('Ending Script ' + program)
+logging.info('Ending Script: ' + program)
 # *************************************************************************************
 # End of Script
 # *************************************************************************************
-
