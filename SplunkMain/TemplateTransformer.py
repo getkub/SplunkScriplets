1233f621e4d83db4779947bc0eaf78b8af786809
getkub <getkub@users.noreply.github.com>
2015-04-22 16:34:52 +0100

    Update TemplateTransformer.py

--- SplunkMain/TemplateTransformer.py
+++ SplunkMain/TemplateTransformer.py
@@ -1,6 +1,6 @@
 import shutil
 import time
-import os, sys
+import os, sys, argparse
 import fnmatch
 import pystache
 import json
@@ -8,16 +8,46 @@ import json
 localtime   = time.localtime()
 timeString  = time.strftime("%Y%m%d%H%M%S", localtime)
 
-dstA = "/tmp/" + "workspace." + timeString
-if not os.path.exists(dstA):
-    os.makedirs(dstA)
+# ----------------------------------------------------------------------
+#
+# This script will transform template into final code when following inputs are given
+#     - Template Source
+#     - configuration data in JSON
+#     - Environment Tag
+#
+# ----------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='Validate Input Config')
+parser.add_argument('source',metavar='sourceDir', help=' - Source Directory with templates')
+parser.add_argument('configs', metavar='configFile', help=' - Absolute Path of Config File')
+parser.add_argument('env',metavar='environment', help=' - Environment like DEV, TEST, PROD')
 
-srcA = "/home/user1/data/mustache/"
-item = "local"
+args = parser.parse_args()
+if not os.path.isfile(args.configs):
+      print('Config file not exists. Quitting... ')
+      sys.exit(1)
+
+if not os.path.exists(args.source):
+      print('Source Template Directory NOT exists. Quitting... ')
+      sys.exit(1)
+
+ENV = args.env
+
+print "===== Start of Script ====="
+
+srcA = args.source
+item = ""
+dstA = "/tmp/" + "workspace." + timeString
+errors = []
 
 # Source and Destination Directories
 src = os.path.join(srcA, item)
 dst = os.path.join(dstA, item)
+
+# print ('SRC = %s' % src)
+# print ('DST = %s' % dst)
+
+#if not os.path.exists(dst):
+#     os.makedirs(dst)
 
 def copyDirectory(src, dst):
     try:
@@ -32,14 +62,18 @@ def copyDirectory(src, dst):
         print('Directory not copied. Error: %s' % e)
         sys.exit(1)
     else:
-        print('Directory Copied Successfully')
+        print('Directories Copied Temporarily to -> %s' % dstA)
 
 copyDirectory(src,dst)
 
 # JSON Configuration File
-jsonConfig = "spnk.json"
+jsonConfig = args.configs
 with open(jsonConfig) as json_file:
     d = json.load(json_file)
+
+# Parse data just for required Environment
+envSpecific = d[ENV]
+# print(envSpecific)
 
 matches = []
 renderer = pystache.Renderer()
@@ -51,10 +85,12 @@ renderer = pystache.Renderer()
 for root, dirnames, filenames in os.walk(dst):
   for filename in fnmatch.filter(filenames, '*.mustache'):
     templateFile = os.path.join(root, filename)
-    matches.append(templateFile)
-    f1=open(os.path.splitext(templateFile)[0], 'w+')
-    f1.write(renderer.render_path(templateFile, d))
+    newFile = os.path.splitext(templateFile)[0]
+    matches.append(newFile)
+    f1=open(newFile, 'w+')
+    f1.write(renderer.render_path(templateFile, envSpecific))
     f1.close()
     os.remove(templateFile)
 
 print "Files Modified => " + str(matches)
+print "===== End of Script ====="
