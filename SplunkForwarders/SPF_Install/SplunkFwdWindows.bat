efa27021fb4fe03aee871e5ffcfd6530f1d0b006
getkub <getkub@users.noreply.github.com>
2015-09-17 17:17:39 +0100

    Update SplunkFwdWindows.bat

--- SplunkForwarders/SPF_Install/SplunkFwdWindows.bat
+++ SplunkForwarders/SPF_Install/SplunkFwdWindows.bat
@@ -2,9 +2,9 @@
 setlocal
 
 REM : User Configurable Variables
-set SplunkUFBuild="6.2.6"
+set SplunkUFBuild=6.2.6
 set LogPath="C:\BuildLogs\SplunkUniversalForwarderInstall.log"
-set productSN="SplunkUF"
+set productSN=SplunkUF
 
 REM : ================================= ======================================
 REM : DO NOT Amend anything below 
@@ -12,12 +12,12 @@ IF "%PROCESSOR_ARCHITECTURE%" == "AMD64" goto b64
 IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" goto b64
 
 :b32
-set SPLUNK_MSI=%~dp0\splunkforwarder-%SplunkUFBuild%-x86-release.msi
+set SPLUNK_MSI=splunkforwarder-%SplunkUFBuild%-x86-release.msi
 REM set above path to 32-bit version
 goto endb6432
  
 :b64
-set SPLUNK_MSI=%~dp0\splunkforwarder-%SplunkUFBuild%-x64-release.msi
+set SPLUNK_MSI=splunkforwarder-%SplunkUFBuild%-x64-release.msi
 REM set above path to 64-bit version
  
 :endb6432
@@ -27,9 +27,10 @@ if not defined ProgramFilesW6432 (
 ) else (
     set LOC=%ProgramFilesW6432%\SplunkUniversalForwarder
 )
- 
+
+echo %date%-%time% Product="%productSN%" Build="%SplunkUFBuild%" Action="FileChecks" FileName="%SPLUNK_MSI%" >> "%LogPath%"
 echo Installing Splunk...
-echo "%date%-%time% Product=%productSN% Build=%SplunkUFBuild% Status=MSI_Install_STARTED" >> "%LogPath%"
+echo %date%-%time% Product="%productSN%" Build="%SplunkUFBuild%" Status="MSI_Install_STARTED" >> "%LogPath%"
 msiexec.exe /i "%SPLUNK_MSI%" INSTALLDIR="%LOC%" AGREETOLICENSE=Yes LAUNCHSPLUNK=0 /QUIET
 
 set msierror=%errorlevel%
@@ -40,26 +41,26 @@ if %msierror%==3010 goto :InstallSuccess
 goto :InstallError
  
 :InstallSuccess
-echo "%date%-%time% Product=%productSN% Build=%SplunkUFBuild% Status=MSI_Install_COMPLETED" ErrorCode=%msierror%" >> "%LogPath%"
+echo %date%-%time% Product="%productSN%" Build="%SplunkUFBuild%" Status="MSI_Install_COMPLETED" ErrorCode="%msierror%" >> "%LogPath%"
 goto :StartConfig
  
 :InstallError
-echo "%date%-%time% Product=%productSN% Build=%SplunkUFBuild% Status=MSI_Install_ERROR ErrorCode=%msierror%" >> "%LogPath%"
+echo %date%-%time% Product="%productSN%" Build="%SplunkUFBuild%" Status="MSI_Install_ERROR" ErrorCode="%msierror%" >> "%LogPath%"
 goto :EndScript
-
 :StartConfig
-
 echo Copying over custom configuration...
-echo "%date%-%time% Product=%productSN% Build=%SplunkUFBuild% Status=Copy_configuration_STARTED" >> "%LogPath%"
+echo %date%-%time% Product="%productSN%" Build="%SplunkUFBuild%" Status="Copy_configuration_STARTED" >> "%LogPath%"
 xcopy "%~dp0\etc" "%LOC%\etc" /s /f /y /r
 pushd "%LOC%\bin"
 echo Starting Splunk...
-echo "%date%-%time% Product=%productSN% Build=%SplunkUFBuild% Status=SplunkUF_Start_STARTED" >> "%LogPath%"
+echo %date%-%time% Product="%productSN%" Build="%SplunkUFBuild%" Status="UFStart_STARTED" >> "%LogPath%"
 splunk start splunkd --accept-license --no-prompt --answer-yes
-echo "%date%-%time% Product=%productSN% Build=%SplunkUFBuild% Status=SplunkUF_Start_COMPLETED" >> "%LogPath%"
+echo %date%-%time% Product="%productSN%" Build="%SplunkUFBuild%" Status="UFStart_COMPLETED" >> "%LogPath%"
+
+echo %date%-%time% Product="%productSN%" Build="%SplunkUFBuild%" Status="SplunkUF Installed & Configured Successfully" >> "%LogPath%"
+
 @echo on
 popd
 endlocal
-
 :EndScript
 Exit %msierror%
