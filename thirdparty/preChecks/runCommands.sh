c1e5c0cc12b34a281b26ebaaaa1abb1b82bfd3ea
getkub <getkub@users.noreply.github.com>
2016-04-29 11:55:02 +0100

    Update runCommands.sh

--- thirdparty/preChecks/runCommands.sh
+++ thirdparty/preChecks/runCommands.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
-# Runs a set of command from CSV file
+# Sample Script for Pre-checks 
+# v0.8
+# Will create run output of various scripts specified in  $paramList
 
 thisHost=`hostname -s`
-outFile=`/tmp/out.${thisHost}.log"
-paramList="/tmp/commands.csv"
+outFile="/tmp/preChecks.${thisHost}.log"
+paramList="params.csv"
+runTime=`date +%F-%T`
 runID=`date +%Y%m%d%H%M%S`
-
 if [ -r ${outFile} ]; then
-  rm $outFile
+   rm $outFile
 fi
 touch ${outFile}
 chmod 777 ${outFile}
-
-# Read line by line and execute
-egrep -v "^#" $paramList | egrep ',' | while read line
-do
- group=`echo $line | awk -F "," '{print $1}'`
- checks=`echo $line | awk -F "," '{print $2}'`
- cmd=`echo $line | awk -F "," '{print $3}'`
- outVar=`eval $cmd 2>/dev/null`
- info="$runID hostName=${thisHost} testGroup=${group} checks=${checks} cmd=${cmd} out=${outVar}"
- echo `date +%F-%T` $info >> $outFile
-done
-
- 
+# Read line by line and execute commands
+egrep -v "^#" ${paramList} | egrep ',' | while read line
+ do 
+   group=`echo $line | awk -F "," '{print $1}'` 
+   checkType=`echo $line | awk -F "," '{print $2}'` 
+   command=`echo $line | tr -d $'\r' | awk -F "," '{print $3}'` 
+   command="timeout 1 bash -c '${command}'"
+   outVar=`eval $command 2>/dev/null;`
+   meta="${runTime} runID=${runID} hostName=${thisHost} testGroup=${group} checkType=${checkType} InputCmd='${command}' Output='${outVar}' "  
+   echo $meta >> ${outFile}
+ done
