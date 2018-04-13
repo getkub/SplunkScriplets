ef992b89cf452ee842d8f420eba702c26c7cd460
getkub <getkub@users.noreply.github.com>
2018-04-13 10:23:05 +0100

    Create scapy_sniff.py

---
+++ thirdparty/python/scapy_sniff.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2.7
+
+# Script to use Scapy and Sniff out traffic based on filter. Similar to wireshark
+
+from scapy.all import *
+
+def pkt_callback(pkt):
+   del pkt[Ether].src
+   del pkt[Ether].dst
+   del pkt[IP].chksum
+   del pkt[UDP].chksum
+   pkt[IP].dst = '192.168.100.100'
+   sendp(pkt)
+
+sniff(iface='lo', filter='port 514', prn=lambda x: x.show(), store=0)
+# filter have lot of options. eg 'udp port 514'
