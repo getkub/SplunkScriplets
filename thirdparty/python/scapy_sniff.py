fb64fccae47ba474034b456aa1a17d64cfec6ad0
getkub <getkub@users.noreply.github.com>
2018-04-13 11:19:55 +0100

    Update scapy_sniff.py

--- thirdparty/python/scapy_sniff.py
+++ thirdparty/python/scapy_sniff.py
@@ -11,6 +11,9 @@ def pkt_callback(pkt):
    del pkt[UDP].chksum
    pkt[IP].dst = '192.168.100.100'
    sendp(pkt)
+   pkt.show()
 
-sniff(iface='lo', filter='port 514', prn=lambda x: x.show(), store=0)
+# sniff(iface='lo', filter='port 514', prn=lambda x: x.show(), store=0)
+sniff(iface='lo', filter='port 514', prn=pkt_callback, store=0)
+
 # filter have lot of options. eg 'udp port 514'
