4569ef1e3e26d049d062d0d2be8c5e63a3a58b7e
getkub <getkub@users.noreply.github.com>
2019-05-01 11:05:33 +0100

    Update simpleConfigParser.py

--- thirdparty/python/simpleConfigParser.py
+++ thirdparty/python/simpleConfigParser.py
@@ -4,4 +4,4 @@ config.read('/tmp/inputs.btool')
 
 for each_section in config.sections():
     for (each_key, each_val) in config.items(each_section):
-        print each_key . ':' . each_val
+        print each_key , ':' , each_val
