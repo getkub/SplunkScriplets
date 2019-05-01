ddadff61658a24cece2763acf6d1bb5a02dd8cf1
getkub <getkub@users.noreply.github.com>
2019-05-01 11:01:24 +0100

    Create simpleConfigParser.py

---
+++ thirdparty/python/simpleConfigParser.py
@@ -0,0 +1,7 @@
+import ConfigParser as configparser
+config = configparser.ConfigParser()
+config.read('/tmp/inputs.btool')
+
+for each_section in config.sections():
+    for (each_key, each_val) in config.items(each_section):
+        print each_key . ':' . each_val
