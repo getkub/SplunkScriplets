a98ec29f6f0428b87a5a3ee2161238cf672d7791
getkub <getkub@users.noreply.github.com>
2021-03-31 16:51:43 +0100

    Update convert_csv_with_double_quotes.ps1

--- thirdparty/powershell/convert_csv_with_double_quotes.ps1
+++ thirdparty/powershell/convert_csv_with_double_quotes.ps1
@@ -1,1 +1,11 @@
 Import-Csv ".\SourceFile.csv" | Export-Csv ".\DestinationFile.csv" -NoTypeInformation
+
+
+
+# To Find duplicates & non-duplicates
+myfile="blah.csv"
+Import-Csv $myfile | group-Object -Property device_ip| Where{$_.Count -gt 1} | Foreach-Object {$_.Group} | Export-csv -Path yes_duplicates.csv -NoTypeInformation
+Import-Csv $myfile | group-Object -Property device_ip| Where{$_.Count -lt 2} | Foreach-Object {$_.Group} | Export-csv -Path non_duplicates.csv -NoTypeInformation
+
+# Then in notepad++ (for duplicates)
+(.+,.+)\r\n.+,.+  => \1
