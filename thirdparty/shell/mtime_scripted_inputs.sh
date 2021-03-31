b0cace2ed8a8fb83077e54ef66ebc20f59a895a2
getkub <getkub@users.noreply.github.com>
2021-03-31 14:46:33 +0100

    Update mtime_scripted_inputs.sh

--- thirdparty/shell/mtime_scripted_inputs.sh
+++ thirdparty/shell/mtime_scripted_inputs.sh
@@ -3,4 +3,4 @@ interval_min=60
 file_input_dir="/tmp"
 file_output_dir="/tmp"
 
-find $file_input_dir -type f -name '*.out' -mmin ${interval_min} -exec stat  --printf="runId=$runId fname=%n fsize=%s atime=%X mtime=%Y ctime=%Z \n" {} \;
+find $file_input_dir -type f -name '*.out' -mmin ${interval_min} -exec stat  --printf="runId=$runId fname=%n fsize=%s atime=%X mtime=%Y ctime=%Z gid=%g gname=%G uid=%u uname=%U acl=%a aclname=%A inode=%i\n" {} \;
