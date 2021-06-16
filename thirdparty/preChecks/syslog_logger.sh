84e6a75d00375f421060f96720dae5948dc2003d
getkub <getkub@users.noreply.github.com>
2021-06-16 14:27:03 +0100

    Update syslog_logger.sh

--- thirdparty/preChecks/syslog_logger.sh
+++ thirdparty/preChecks/syslog_logger.sh
@@ -1,6 +1,8 @@
-# syslog simulation using logger
+# syslog simulation using logger or netcat (nc)
 remoteIP="10.12.13.14"
 remotePort="514"
 sampleMessage="TEST_MSG"
 
-logger -n $remoteIP -P $remotePort $sampleMessage
+# logger -n $remoteIP -P $remotePort $sampleMessage
+
+echo $sampleMessage | nc $remoteIP $remotePort
