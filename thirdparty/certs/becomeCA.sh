1163ac5af912db3d363f315a7c28579eb9dcfe6c
getkub <kinbsk@gmail.com>
2022-01-02 14:22:59 +0000

    cert gen

---
+++ thirdparty/certs/becomeCA.sh
@@ -0,0 +1,39 @@
+######################
+# Become a Certificate Authority
+######################
+# https://stackoverflow.com/questions/7580508/getting-chrome-to-accept-self-signed-localhost-certificate
+certDir="/tmp/gen_cert"
+mkdir -p ${certDir}
+caConf=${certDir}/ca.conf
+certConf=${certDir}/cert.conf
+
+if [ ! -f "$caConf" ] || [ ! -f "$certConf" ]; then
+    echo "Config File: $caConf OR $certConf NOT found.. Exiting"
+    exit 10
+fi
+
+# echo " *** Generate private key *** "
+# openssl genrsa -des3 -out ${certDir}/myCA.key 2048
+# echo " *** Generate root certificate *** "
+# openssl req -x509 -new -nodes -key ${certDir}/myCA.key -sha256 -days 825 -out ${certDir}/myCA.pem
+
+echo " *** Generate CA key & CA pem *** "
+openssl req -x509 -newkey rsa:4096 -keyout ${certDir}/myCA.key -out ${certDir}/myCA.pem -config $caConf -days 10000 -nodes
+
+
+######################
+# Create CA-signed certs
+######################
+
+NAME=mydev.test # Use your own domain name
+echo " *** Generate a private key *** "
+openssl genrsa -out ${certDir}/$NAME.key 2048
+echo " *** Create a certificate-signing request *** "
+openssl req -new -key ${certDir}/$NAME.key -out ${certDir}/$NAME.csr -config $certConf -nodes
+echo " *** Create a config file for the extensions *** "
+echo ""
+echo " *** Create the signed certificate *** "
+openssl x509 -req -in ${certDir}/$NAME.csr -CA ${certDir}/myCA.pem -CAkey ${certDir}/myCA.key -CAcreateserial \
+-out ${certDir}/$NAME.crt -days 825 -sha256
+
+echo "Certificates are now ready at ${certDir}/ "
--- thirdparty/certs/openssl.md
+++ thirdparty/certs/openssl.md
@@ -1,31 +1,44 @@
 # To find keys in a pem key . Also good to check/verify the passphrase supplied
+```
 openssl rsa -in /opt/splunk/etc/auth/server.pem -text
-
+```
 # TO Check connectivity similar to Curl
+```
 openssl s_client -connect  <remoteServer>:<port>
-
-
 openssl x509 req -config <my_config_key> in <server.key> -out <my_cert.crt> - days 3650
+```
 
 # Generate NEW CERT directly from config file (for self-signed)
+```
 openssl req -x509  -new -config <my_config_key> in <server.key> -out <my_cert.crt> - days 3650
+```
 
 # For existing
+```
 openssl req -x509  -new -config <my_config_key> -key <server.key> -out <my_cert.crt> - days 3650
-
-
-
+```
 
 ## To check if Key matches with cert and both md5sum should match
+```
 keyfile="/tmp/file/file.key"
 cert="/tmp/file/mycert.crt"
 openssl rsa -modulus -noout -in $keyfile | openssl md5
 openssl x509 -modulus -noout -in $cert | openssl md5
-
+```
 
 # Convert to pkcs8
+```
 keyfile="/tmp/file/file.key"
 password_of_pk8="mypass"
 pkcs8file="/tmp/file/file.pk8"
 openssl pkcs8 -in $keyfile -topk8 -passout pass:${password_of_pk8} -out  $pkcs8file
+```
 
+## Checking
+```
+#  Checking whether the hostname on the certificate matches the name you want
+openssl s_client -verify_hostname example.com  -connect serverfault.com:443
+
+# Checking whether the certificate is from a trusted CA
+openssl s_client -verify 2 -connect serverfault.com:443
+```
