1eab81c67f3a49c5230ec8a4b7afdabbb6e2d27b
getkub <kinbsk@gmail.com>
2022-01-02 14:57:14 +0000

    cert gen

--- thirdparty/certs/becomeCA.sh
+++ thirdparty/certs/becomeCA.sh
@@ -18,7 +18,7 @@ fi
 # openssl req -x509 -new -nodes -key ${certDir}/myCA.key -sha256 -days 825 -out ${certDir}/myCA.pem
 
 echo " *** Generate CA key & CA pem *** "
-openssl req -x509 -newkey rsa:4096 -keyout ${certDir}/myCA.key -out ${certDir}/myCA.pem -config $caConf -days 10000 -nodes
+openssl req -x509 -newkey rsa:4096 -keyout ${certDir}/myCA.key -out ${certDir}/myCA.pem -config $caConf -days 3650 -nodes
 
 
 ######################
@@ -30,10 +30,8 @@ echo " *** Generate a private key *** "
 openssl genrsa -out ${certDir}/$NAME.key 2048
 echo " *** Create a certificate-signing request *** "
 openssl req -new -key ${certDir}/$NAME.key -out ${certDir}/$NAME.csr -config $certConf -nodes
-echo " *** Create a config file for the extensions *** "
-echo ""
 echo " *** Create the signed certificate *** "
 openssl x509 -req -in ${certDir}/$NAME.csr -CA ${certDir}/myCA.pem -CAkey ${certDir}/myCA.key -CAcreateserial \
--out ${certDir}/$NAME.crt -days 825 -sha256
+-out ${certDir}/$NAME.crt -days 365 -sha256 -extensions 'my_server_exts'
 
 echo "Certificates are now ready at ${certDir}/ "
