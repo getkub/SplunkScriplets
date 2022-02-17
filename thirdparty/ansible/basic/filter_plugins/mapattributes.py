588c7e07df5fb74c28cbe2c16b523a374019c72e
getkub <kinbsk@gmail.com>
2022-02-17 12:59:15 +0000

    Updated map filters

---
+++ thirdparty/ansible/basic/filter_plugins/mapattributes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+class FilterModule(object):
+  def filters(self):
+    return { 'mapattributes': self.mapattributes }
+
+  def mapattributes(self, list_of_dicts, list_of_keys):
+    l = []
+    for di in list_of_dicts:
+      newdi = { }
+      for key in list_of_keys:
+        newdi[key] = di[key]
+      l.append(newdi)
+    return l
