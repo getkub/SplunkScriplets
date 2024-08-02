881ca03340a4e99a45989da4df33cc19dcf8eee4
getkub <getkub@users.noreply.github.com>
2024-08-02 09:16:48 +0100

    Update directoryPath.sh

--- thirdparty/shell/directoryPath.sh
+++ thirdparty/shell/directoryPath.sh
@@ -1,3 +1,5 @@
+# https://stackoverflow.com/questions/242538/unix-shell-script-find-out-which-directory-the-script-file-resides
+
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 CONFIG_FILE_DIR="$SCRIPTPATH/../../.."
