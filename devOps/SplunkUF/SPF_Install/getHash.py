6349462523ec00c6f3084f449538e2edad050355
getkub <kinbsk@gmail.com>
2025-05-09 00:32:39 +0100

    Updated scraper

--- devOps/SplunkUF/SPF_Install/getHash.py
+++ devOps/SplunkUF/SPF_Install/getHash.py
@@ -1,20 +1,54 @@
 import requests
-from bs4 import BeautifulSoup
 import re
+from collections import defaultdict
 
-version = "6.6.3"
-os = "linux"
-arch = "amd64"
-pkg = "deb"
-url = f"https://www.splunk.com/en_us/download/previous-releases.html"
+REPO = "splunk/splunk"
+URL = f"https://hub.docker.com/v2/repositories/{REPO}/tags?page_size=100"
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-links = soup.find_all("a", href=re.compile(f"splunk-{version}-.*-{os}.*{arch}\.{pkg}"))
+# Match tags like 9.4.2 (not 9.4 or latest)
+version_re = re.compile(r'^\d+\.\d+\.\d+$')
+hash_re = re.compile(r'^[a-f0-9]{12}$')
 
-for link in links:
-    href = link["href"]
-    match = re.search(r"splunk-\d+\.\d+\.\d+-([a-f0-9]+)-", href)
-    if match:
-        hash_value = match.group(1)
-        print(f"Found hash: {hash_value}")
+def fetch_all_tags():
+    tags = []
+    url = URL
+    while url:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        tags.extend(data["results"])
+        url = data.get("next")
+    return tags
+
+def group_tags_by_digest(tags):
+    digest_map = defaultdict(list)
+    for tag in tags:
+        name = tag["name"]
+        images = tag.get("images", [])
+        if images:
+            digest = images[0].get("digest")
+            if digest:
+                digest_map[digest].append(name)
+    return digest_map
+
+def extract_version_hash_pairs(digest_map):
+    pairs = []
+    for tags in digest_map.values():
+        versions = [t for t in tags if version_re.match(t)]
+        hashes = [t for t in tags if hash_re.match(t)]
+        for version in versions:
+            for h in hashes:
+                pairs.append((version, h))
+    return pairs
+
+def main():
+    tags = fetch_all_tags()
+    digest_map = group_tags_by_digest(tags)
+    pairs = extract_version_hash_pairs(digest_map)
+
+    print("version,hash")
+    for version, h in sorted(pairs):
+        print(f"{version},{h}")
+
+if __name__ == "__main__":
+    main()
